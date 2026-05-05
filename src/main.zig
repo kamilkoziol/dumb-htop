d9b4acd4a3f0430c9bafdd517387f587adc7f621
Kamil Koziol <kamkoziol1@gmail.com>
2026-05-05 21:21:20 +0200

    sorting threads and tries to make it responsive

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,28 @@
 const std = @import("std");
 
-// const Ansi = enum {
-//     reset,
-//     clear,
-//     red,
-//     green,
-//     yellow,
-//     cyan,
-//
-//     pub fn format(self: Ansi, comptime _: []const u8, _: std.fmt.FormatOptions, writer: anytype) !void {
-//         try writer.writeAll(switch (self) {
-//             .reset  => "\x1b[0m",
-//             .clear  => "\x1b[2J\x1b[H",
-//             .red    => "\x1b[31m",
-//             .green  => "\x1b[32m",
-//             .yellow => "\x1b[33m",
-//             .cyan   => "\x1b[36m",
-//         });
-//     }
-// };
+const Ansi = enum {
+    reset,
+    clear,
+    red,
+    green,
+    yellow,
+    cyan,
+    altOn,
+    altOff,
+
+    pub fn format(self: Ansi, writer: *std.Io.Writer) !void {
+        try writer.writeAll(switch (self) {
+            .reset => "\x1b[0m",
+            .clear => "\x1b[2J\x1b[H",
+            .red => "\x1b[31m",
+            .green => "\x1b[32m",
+            .yellow => "\x1b[33m",
+            .cyan => "\x1b[36m",
+            .altOn => "\x1b[?1049h",
+            .altOff => "\x1b[?1049l",
+        });
+    }
+};
 
 const IDLE_INDEX = 3;
 const IOWAIT_INDEX = 4;
@@ -28,7 +32,23 @@ const ThreadTimes = struct {
     idle: u32,
 };
 
-fn readProc(alloc: std.mem.Allocator, io: std.Io) !std.StringHashMap(ThreadTimes) {
+const TerminalSize = struct {
+    width: u16,
+    height: u16,
+};
+
+fn getTerminalSize() TerminalSize {
+    var winSize: std.posix.winsize = undefined;
+    const ret = std.os.linux.ioctl(std.posix.STDOUT_FILENO, std.posix.T.IOCGWINSZ, @intFromPtr(&winSize));
+    const err = std.os.linux.errno(ret);
+    const size = switch (err) {
+        .SUCCESS => TerminalSize{ .height = winSize.row, .width = winSize.col },
+        else => TerminalSize{ .height = 30, .width = 40 },
+    };
+
+    return size;
+}
+fn readProc(alloc: std.mem.Allocator, io: std.Io) !std.AutoHashMap(u16, ThreadTimes) {
     const f = std.Io.Dir.openFileAbsolute(io, "/proc/stat", .{ .mode = .read_only }) catch |err| {
         std.debug.print("error open {any}", .{err});
         return err;
@@ -40,7 +60,7 @@ fn readProc(alloc: std.mem.Allocator, io: std.Io) !std.StringHashMap(ThreadTimes
 
     var file_reader = f.reader(io, file_buffer);
 
-    var map: std.StringHashMap(ThreadTimes) = .init(alloc);
+    var map: std.AutoHashMap(u16, ThreadTimes) = .init(alloc);
 
     while (file_reader.interface.takeDelimiterExclusive('\n')) |line| {
         file_reader.interface.toss(1);
@@ -63,7 +83,7 @@ fn readProc(alloc: std.mem.Allocator, io: std.Io) !std.StringHashMap(ThreadTimes
         const key = iter.next() orelse {
             continue;
         };
-        const owned_key = try alloc.dupe(u8, key);
+        const threadNumber = try std.fmt.parseUnsigned(u16, key[3..], 10);
         while (iter.next()) |val| : (i += 1) {
             const int_val = std.fmt.parseUnsigned(u32, val, 10) catch |err| {
                 std.debug.print("parseint error: {any} value :>>{s}<<\n", .{ err, val });
@@ -75,7 +95,7 @@ fn readProc(alloc: std.mem.Allocator, io: std.Io) !std.StringHashMap(ThreadTimes
                 busy += int_val;
             }
         }
-        try map.put(owned_key, .{ .idle = idle, .busy = busy });
+        try map.put(threadNumber, .{ .idle = idle, .busy = busy });
     } else |err| {
         switch (err) {
             error.EndOfStream => {},
@@ -87,66 +107,64 @@ fn readProc(alloc: std.mem.Allocator, io: std.Io) !std.StringHashMap(ThreadTimes
     return map;
 }
 
-fn cleanupMap(map: *std.StringHashMap(ThreadTimes), alloc: std.mem.Allocator) void {
-    var keyIterator = map.keyIterator();
-    while (keyIterator.next()) |key| {
-        alloc.free(key.*);
-    }
+fn cleanupMap(map: *std.AutoHashMap(u16, ThreadTimes)) void {
     map.deinit();
 }
 
-fn visualizeData(io: std.Io, prev: std.StringHashMap(ThreadTimes), current: std.StringHashMap(ThreadTimes)) void {
-    var stdout_buffer: [1024]u8 = undefined;
-    var stdout_writer = std.Io.File.stdout().writer(io, &stdout_buffer);
-    const stdout = &stdout_writer.interface;
-
-    var keyIterator = current.keyIterator();
-    while (keyIterator.next()) |key| {
-        const currentValue = current.get(key.*).?;
-        const prevValue = prev.get(key.*).?;
-        printBar(key.*, prevValue, currentValue, stdout);
+fn visualizeData(writer: *std.Io.Writer, prev: std.AutoHashMap(u16, ThreadTimes), current: std.AutoHashMap(u16, ThreadTimes)) void {
+    var i: u16 = 0;
+    while (i < current.count()) : (i += 1) {
+        const currentValue = current.get(i);
+        const prevValue = prev.get(i);
+        printBar(i, prevValue, currentValue, writer);
     }
-    stdout.flush() catch unreachable;
 }
 
-fn printBar(key: []const u8, prev: ThreadTimes, current: ThreadTimes, stdout: *std.Io.Writer) void {
-    const busy: f32 = @floatFromInt(current.busy - prev.busy);
-    const idle: f32 = @floatFromInt(current.idle - prev.idle);
+fn printBar(key: u16, prev: ?ThreadTimes, current: ?ThreadTimes, stdout: *std.Io.Writer) void {
+    const prevValue = prev orelse return;
+    const currentValue = current orelse return;
+    const busy: f32 = @floatFromInt(currentValue.busy - prevValue.busy);
+    const idle: f32 = @floatFromInt(currentValue.idle - prevValue.idle);
     const total = busy + idle;
     const percentage = busy / total * 100;
 
     const barCharacter = '|';
     const emptyCharacter = ' ';
 
-    const barLength: u8 = 10;
+    const barLength: u8 = 50;
     const filled: u8 = @intFromFloat(busy / total * barLength);
 
     var bar: [barLength]u8 = undefined;
     @memset(bar[0..filled], barCharacter);
     @memset(bar[filled..], emptyCharacter);
 
-    stdout.print("{s}[{s}] {d:.1}%\n", .{ key[3..], bar, percentage }) catch unreachable;
+    stdout.print("{d}[{s}] {d:.1}%\n", .{ key, bar, percentage }) catch unreachable;
 
     return;
 }
 
 pub fn main(init: std.process.Init) !void {
-    var gpa: std.heap.DebugAllocator(.{}) = .init;
-    defer _ = gpa.deinit();
+    const alloc = init.gpa;
 
-    const alloc = gpa.allocator();
-
-    var prev: ?std.StringHashMap(ThreadTimes) = null;
+    var prev: ?std.AutoHashMap(u16, ThreadTimes) = null;
     var current = try readProc(alloc, init.io);
     defer {
-        cleanupMap(&current, alloc);
-        if (prev) |*p| cleanupMap(p, alloc);
+        cleanupMap(&current);
+        if (prev) |*p| cleanupMap(p);
     }
+
+    var stdout_buffer: [1024]u8 = undefined;
+    var stdout_writer = std.Io.File.stdout().writer(init.io, &stdout_buffer);
+    const stdout = &stdout_writer.interface;
+
+    stdout.print("{f}", .{Ansi.altOn});
+
     while (true) {
         try std.Io.sleep(init.io, std.Io.Duration.fromSeconds(1), std.Io.Clock.real);
-        if (prev) |*p| cleanupMap(p, alloc);
+        if (prev) |*p| cleanupMap(p);
         prev = current;
         current = try readProc(alloc, init.io);
-        visualizeData(init.io, prev.?, current);
+        visualizeData(stdout, prev.?, current);
+        try stdout.flush();
     }
 }
